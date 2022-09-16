9ed6f1764c5f4c80fafd265d6d84d3c921953dfa
NaorNC <81817039+NaorNC@users.noreply.github.com>
2022-09-16 04:22:51 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+-- Discord.gg/NCHub
+fx_version 'adamant'
+game 'gta5'
+
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+    'vehiclelayouts.meta'
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
